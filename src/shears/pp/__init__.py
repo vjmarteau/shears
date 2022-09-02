3e7427c0db18f2ed0f05fc16b0a5f514cec54d93
Gregor Sturm <mail@gregor-sturm.de>
2022-09-02 20:37:41 +0200

    Basic implementation of shears

--- src/shears/pp/__init__.py
+++ src/shears/pp/__init__.py
@@ -1,9 +1,20 @@
+import itertools
 import warnings
+from typing import Optional
 
 import anndata
+import numpy as np
+import pandas as pd
 import scanpy as sc
 import sklearn.preprocessing
 from anndata import AnnData
+from sklearn.linear_model import Ridge
+from threadpoolctl import threadpool_limits
+
+# TODO widget doesn't work in vscode :(
+# from tqdm.auto import tqdm
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 
 def quantile_norm(adata: AnnData, *, layer=None, key_added="quantile_norm"):
@@ -34,13 +45,48 @@ def recipe_shears(adata_sc, adata_bulk, *, n_top_genes=2000, layer_sc=None, laye
     return adata_sc, adata_bulk
 
 
-def cell_weights(adata_sc, adata_bulk, *, inplace=True, key_added="cell_weights"):
+@threadpool_limits.wrap(limits=1)
+def _deconvolute(bulk_sample, sc_mat, alpha, random_state):
+    model = Ridge(alpha=alpha, positive=True, random_state=random_state)
+    fit = model.fit(sc_mat, bulk_sample)
+    return fit.coef_
+
+
+def cell_weights(
+    adata_sc,
+    adata_bulk,
+    *,
+    inplace=True,
+    alpha_callback=lambda adata_sc: adata_sc.shape[0],
+    layer_sc="quantile_norm",
+    layer_bulk="quantile_norm",
+    key_added="cell_weights",
+    random_state=0,
+    n_jobs=None,
+) -> Optional[pd.DataFrame]:
     """
     Computes a bulk_sample x cell matrix assigning each cell a weight for each bulk sample.
 
     This is conceptually similar to deconvolution, except that instead of a signature matrix,
     we have a single-cell dataset.
 
-    If inplace is True, stores the resulting matrix in adata_bulk.obsm[key_added]
+    If inplace is True, stores the resulting matrix in adata_sc.obsm[key_added]
     """
-    raise NotImplementedError
+    res = process_map(
+        _deconvolute,
+        (adata_bulk.layers[layer_bulk][i, :] for i in range(adata_bulk.shape[0])),
+        itertools.repeat(adata_sc.layers[layer_sc].T),
+        itertools.repeat(alpha_callback(adata_sc)),
+        itertools.repeat(random_state),
+        max_workers=n_jobs,
+        chunksize=10,
+        tqdm_class=tqdm,
+        total=adata_bulk.shape[0],
+    )
+
+    res = pd.DataFrame(np.array(res).T, index=adata_sc.obs_names, columns=adata_bulk.obs_names)
+
+    if inplace:
+        adata_sc.obsm[key_added] = res
+    else:
+        return res
--- src/shears/tl/__init__.py
+++ src/shears/tl/__init__.py
@@ -1,7 +1,58 @@
-def shears(adata_sc, adata_bulk, *, inplace=True, key_added="shears"):
+import itertools
+
+import pandas as pd
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+
+
+def _test_cell(formula, df):
+    mod = smf.glm(formula, data=df, family=sm.families.Binomial())
+    res = mod.fit()
+    return res.pvalues["cell_weight"], res.params["cell_weight"]
+
+
+def shears(
+    adata_sc,
+    adata_bulk,
+    *,
+    dep_var,
+    covariate_str="",
+    inplace=True,
+    cell_weights_key="cell_weights",
+    key_added="shears",
+    n_jobs=None,
+):
     """
     Compute score for each cell.
 
     Stores the scores in adata_sc.obs[key_added] unless inplace is False.
+
+    Parameters
+    ----------
+    covariate_str
+        covariates to add, e.g. `+ age + C(gender)`
     """
-    raise NotImplementedError
+    formula = f"{dep_var} ~ cell_weight {covariate_str}"
+    keep_cols = [c for c in adata_bulk.obs.columns if c in dep_var + covariate_str]
+    assert "cell_weight" not in keep_cols, "cell_weight is a reserved column name"
+    bulk_obs = adata_bulk.obs.loc[:, keep_cols].copy()
+
+    def _df_iter():
+        for c in adata_sc.obs_names:
+            tmp_df = bulk_obs.copy()
+            tmp_df["cell_weight"] = adata_sc.obsm[cell_weights_key].loc[c, :]
+            yield tmp_df
+
+    res = process_map(
+        _test_cell,
+        itertools.repeat(formula),
+        _df_iter(),
+        total=adata_sc.shape[0],
+        chunksize=100,
+        max_workers=n_jobs,
+        tqdm_class=tqdm,
+    )
+
+    return pd.DataFrame(res, index=adata_sc.obs_names, columns=["pvalue", "coef"])
