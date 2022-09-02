5780c94d5fad25301b812d79161a6ea4f0c93e88
Gregor Sturm <mail@gregor-sturm.de>
2022-09-02 10:29:10 +0200

    Implement preprocessing

--- src/shears/pp/__init__.py
+++ src/shears/pp/__init__.py
@@ -1,20 +1,37 @@
+import warnings
+
+import anndata
+import scanpy as sc
+import sklearn.preprocessing
 from anndata import AnnData
 
 
-def quantile_norm(adata: AnnData, layer=None, key_added="quantile_norm"):
+def quantile_norm(adata: AnnData, *, layer=None, key_added="quantile_norm"):
     """Perform quantile normalization on AnnData object
 
     Stores the normalized data in a new layer with the key `key_added`.
     """
-    raise NotImplementedError
+    X = adata.X if layer is None else adata.layers[layer]
+    adata.layers[key_added] = sklearn.preprocessing.quantile_transform(X)
 
 
-def preprocess_shears(adata_sc, adata_bulk):
-    """Automatically apply all preprocessing steps (highly variable genes, subsetting, quantile normalization).
-
-    Convenient wrapper...
+def recipe_shears(adata_sc, adata_bulk, *, n_top_genes=2000, layer_sc=None, layer_bulk=None, key_added="quantile_norm"):
     """
-    raise NotImplementedError
+    Automatically preprocess data for shears.
+
+    Single-cell input data is expected to contain raw counts.
+    Bulk data is expected to contain TPM (or any other measure metric for gene length).
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", anndata.ImplicitModificationWarning)
+        adata_sc = adata_sc[:, adata_sc.var_names.isin(adata_bulk.var_names)]
+        sc.pp.highly_variable_genes(adata_sc, n_top_genes=n_top_genes, layer=layer_sc, flavor="seurat_v3", subset=True)
+        adata_bulk = adata_bulk[:, adata_sc.var_names]
+
+    quantile_norm(adata_sc, layer=layer_sc, key_added=key_added)
+    quantile_norm(adata_bulk, layer=layer_bulk, key_added=key_added)
+
+    return adata_sc, adata_bulk
 
 
 def cell_weights(adata_sc, adata_bulk, *, inplace=True, key_added="cell_weights"):
