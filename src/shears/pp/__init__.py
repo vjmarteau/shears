8e13262f74b596e54f1d33f09a67166b01b77bce
Gregor Sturm <mail@gregor-sturm.de>
2022-09-02 09:47:31 +0200

    Draft API

--- src/shears/pp/__init__.py
+++ src/shears/pp/__init__.py
@@ -1,1 +1,29 @@
-from .basic import basic_preproc
+from anndata import AnnData
+
+
+def quantile_norm(adata: AnnData, layer=None, key_added="quantile_norm"):
+    """Perform quantile normalization on AnnData object
+
+    Stores the normalized data in a new layer with the key `key_added`.
+    """
+    raise NotImplementedError
+
+
+def preprocess_shears(adata_sc, adata_bulk):
+    """Automatically apply all preprocessing steps (highly variable genes, subsetting, quantile normalization).
+
+    Convenient wrapper...
+    """
+    raise NotImplementedError
+
+
+def cell_weights(adata_sc, adata_bulk, *, inplace=True, key_added="cell_weights"):
+    """
+    Computes a bulk_sample x cell matrix assigning each cell a weight for each bulk sample.
+
+    This is conceptually similar to deconvolution, except that instead of a signature matrix,
+    we have a single-cell dataset.
+
+    If inplace is True, stores the resulting matrix in adata_bulk.obsm[key_added]
+    """
+    raise NotImplementedError
--- src/shears/tl/__init__.py
+++ src/shears/tl/__init__.py
@@ -1,1 +1,7 @@
-from .basic import basic_tool
+def shears(adata_sc, adata_bulk, *, inplace=True, key_added="shears"):
+    """
+    Compute score for each cell.
+
+    Stores the scores in adata_sc.obs[key_added] unless inplace is False.
+    """
+    raise NotImplementedError
